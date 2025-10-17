0b84099ea8de8d3342179445666813d2308cdbe2
Nia ramadani <ramadahaninia514@gmail.com>
2025-10-18 01:26:52 +0700

    PUSH pertama saya untuk project laravel di github

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', action: function ()  {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('/profil', action: function () {
+    $nama = 'nia';
+    return view('profil',data: compact('nama'));
+    //return view('profil)->with('nama,nama));
+});
