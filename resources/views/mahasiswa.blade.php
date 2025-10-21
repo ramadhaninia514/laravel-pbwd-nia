747434f7156e348791d4c395eadf80d3400c89d5
Nia ramadani <ramadahaninia514@gmail.com>
2025-10-21 23:21:45 +0700

    belajar mengenai tabel

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -3,18 +3,78 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>profil</title>
+    <title>mahasiswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-  <h1>halaman profil</h1>  
-  <p>nama saya adalah {{$nama;}}</p>
-  <ul>
-    <li><a href="/home"></a>home</li>
-    <li><a href="/about">about</a></li>
-    <li><a href="/mahasiswa">mahasiswa</a></li>
-    <li>profile</li>
-  </ul>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">laravel</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/mahasiswa">mahasiswa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/profil">profil</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="/about">about</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container" style="margin-left: 30px">
+   <h1>ini adalah halaman mahasiswa</h1> 
+
+   {{-- 
+     ROWSPAN = untuk menggabungkan baris
+     COLSPAN = untuk menggabungkan kolom
+   --}}
+
+   <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
+    <thead>
+      <tr>
+        <th>NPM</th>
+        <th>Nama mahasiswa</th>
+        <th>jenis kelamin</th>
+        <th colspan="2">TTL</th>  
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>123</td>
+        <td>Fulan</td>
+        <td>Laki-laki</td>
+        <td>30-09-05</td>
+        <td rowspan="3">Kota medan</td>
+      </tr>
+      <tr>
+        <td>123</td>
+        <td>Fuji</td>
+        <td>Laki-laki</td>
+        <td>30-09-05</td>
+      </tr>
+      <tr>
+        <td>123</td>
+        <td>Fuso</td>
+        <td>Laki-laki</td>
+        <td>30-09-05</td>
+      </tr>
+    </tbody>
+   </table>
+</div>
+   
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
